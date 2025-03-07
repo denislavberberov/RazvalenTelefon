d40ac27f66f376f6d49c44f5c077d1d34dde4f8f
deniksb <diberberov@diadraw.com>
2025-03-07 10:40:31 +0200

    Init2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import letTheGameBegin from './assets/let-the-game-begin-jigsaw.gif';
+
+function App() {
+  return (
+    <div className="App" style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh'}}>
+      <img src={letTheGameBegin} style={{width: '500px'}}></img>
+      <button className="fancy-button">
+      <a href="https://www.youtube.com/watch?v=_sX1wRQB6jE" target="_blank" rel="noopener noreferrer">
+        КЛИКНИ ТУК
+      </a>
+    </button>
+
+    </div>
+  );
+}
+
+export default App;
